a58de0c4e2d853c8f77dc92d1f5519abb4ec75c7
liyao <regreceive@163.com>
2018-06-08 11:11:16 +0800

    白皮书

--- src/routes/home/Banner/locales/enUS.js
+++ src/routes/home/Banner/locales/enUS.js
@@ -2,7 +2,7 @@ const dicts = {
   title:
     '<p>Safe and stable distributed encrypted</p><p>ledgers and a future-oriented</p>',
   whitePaper: 'White Paper',
-  link: 'http://echo.center/data/ECHO-whitepaper03.pdf',
+  link: 'https://www.echoex.io/data/ECHO_WhitePaper_en.pdf',
 };
 
 export default dicts;
--- src/routes/home/Banner/locales/zhCN.js
+++ src/routes/home/Banner/locales/zhCN.js
@@ -1,7 +1,7 @@
 const dicts = {
   title: '<p>安全稳定的分布式加密账本&</p><p>面向未来的分布式商业生态</p>',
   whitePaper: '白 皮 书',
-  link: 'http://echo.center/data/ECHO-whitepaper01.pdf',
+  link: 'https://www.echoex.io/data/ECHO_WhitePaper_cn.pdf',
 };
 
 export default dicts;
